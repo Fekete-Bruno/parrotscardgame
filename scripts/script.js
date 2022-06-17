40c11b84aeeb7a4602a632464ea5bd3044780948
Fekete-Bruno <brunohffekete@gmail.com>
2022-06-17 12:03:31 -0300

    V7:scoreboard working and preparing for game restart

--- scripts/script.js
+++ scripts/script.js
@@ -8,23 +8,36 @@ const cardDeck = ["bobrossparrot","bobrossparrot",
             ];
 const gifDeck = [];
 const cards = [];
-let score = 0;
-let card1='';
-let card2='.';
+let score;
+let scoreboard = document.querySelector("footer");
+let card1;
+let card2;
 let amount;
-let correctAmount = 0;
+let correctAmount;
 
 
 function gameStarter(){
+
+    // Resets everything for a new game
+    score = 0;
+    scoreboard.innerHTML = `SCORE: ${score}`;
+    card1='';
+    card2='.';
+    correctAmount = 0;
+    amount = 0;
+
+    // Asks amount of cards
     while(amount<4 || amount > 14 || amount%2 == 1 || isNaN(amount)) { 
         amount = parseInt(prompt('Com quantas cartas você quer jogar? (Por favor escolha um número par entre 4 e 14)'));
     }
     let ul = document.querySelector(".game-board");
     ul.innerHTML = '';
+
     // Preparing cards with the right amount
     for (let index = 0; index < amount; index++) {
         cards.push(cardDeck[index])
     }
+    
     console.log(cards); // Log unshuffled array
 
     // Everiday I'm Shufflin'
@@ -38,7 +51,8 @@ function gameStarter(){
                             </div>
                         `;
     }
-    
+    score = 0;
+    scoreboard.innerHTML = `SCORE: ${score}`;
     alert('Game Start!');
 }
 
@@ -49,15 +63,18 @@ function turn (card) {
     card.classList.add("front");
     checkCards(card);
     score++;
+    scoreboard.innerHTML = `SCORE: ${score}`;
     console.log(`Score: ${score}`)
  }
+
+ // Checks if there are two turned cards, and checks if they are equal.
 function checkCards(card) {
     if(card1===''){
         card1 = card;
     } else {
         card2 = card;
     }
-    
+
 }
 
 // Array sort
